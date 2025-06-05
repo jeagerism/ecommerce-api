b60dc93c64434a25695de3c6fabb856f87dae86b
jeagerism <oo4dxuser@gmail.com>
2025-06-05 13:07:41 +0700

    second feature

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello Mother Fucker")
+	var age int
+	var name string
+
+	fmt.Print("กรอกชื่อและอายุ (เว้นด้วยช่องว่าง): ")
+	fmt.Scanln(&name, &age)
+
+	fmt.Printf("ชื่อ: %s, อายุ: %d ปี\n", name, age)
 }
