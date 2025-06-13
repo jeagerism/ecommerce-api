2257c23ef5157419466c79b94a552f968ec01688
jeagerism <oo4dxuser@gmail.com>
2025-06-13 19:36:21 +0700

    fix port

--- main.go
+++ main.go
@@ -103,7 +103,12 @@ func main() {
 	orderShopGroup.Use(middleware.RequireShopRole()) // เฉพาะ shop เท่านั้นที่เข้าถึงได้
 	shopOrderDel.NewShopOrderHandler(orderShopGroup, orderUsecase.NewOrderUsecase(orderRepo.NewOrderRepository(DB)))
 
-	e.Logger.Fatal(e.Start(":8080"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	e.Logger.Fatal(e.Start(":" + port))
+
 }
 
 func newDB() (*gorm.DB, error) {
@@ -114,10 +119,11 @@ func newDB() (*gorm.DB, error) {
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("SSL_MODE")
 
 	connString := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname,
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode,
 	)
 
 	return gorm.Open(postgres.Open(connString), &gorm.Config{})
